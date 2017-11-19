82520d9ddccb6f025044ce083873e6758da893ad
Hanson-Tyler <tyler.hanson123@gmail.com>
2017-11-19 01:32:48 -0800

    update

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = express();
 var pg = require("pg"); // This is the postgres database connection module.
 const { Client } = require('pg');
 
+var connectionString = "postgres://gudqbffhpdwnmx:2b1f8bb5a9e04fb53f09047dc0ff21a2c460ad5991b90fa1bdb7376db807dbfd@ec2-107-20-255-96.compute-1.amazonaws.com:5432/dfk000085n7kj8";
+const client = new pg.Client(connectionString);
+client.connect()
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -12,19 +16,19 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var connectionString = "postgres://gudqbffhpdwnmx:2b1f8bb5a9e04fb53f09047dc0ff21a2c460ad5991b90fa1bdb7376db807dbfd@ec2-107-20-255-96.compute-1.amazonaws.com:5432/postgresql-asymmetrical-63190";
-
 app.get('/getPerson/:id', function(request, response) {
-    pg.connect(connectionString, function(err, client, done){
-          client.query('SELECT * FROM messages;', (err, res) => {
-        if (err) throw err;
+
+    client.query('SELECT * FROM messages;', (err, res) => {
+        if (err) 
+          throw err;
         for (let row of res.rows) {
         console.log(JSON.stringify(row));
-      }
-    })
-       
+        }
+   
+        client.end();
+
         response.render('pages/messages', {"data": res.rows})
-});
+});   
 });
 
 app.listen(app.get('port'), function() {
