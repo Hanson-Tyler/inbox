31f264e556538ee2a64992eade7f06a61bc94247
Hanson-Tyler <tyler.hanson123@gmail.com>
2017-11-19 01:05:58 -0800

    updating milestone

--- index.js
+++ index.js
@@ -12,22 +12,18 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-const client = new Client({
-  connectionString: "postgres://gudqbffhpdwnmx:2b1f8bb5a9e04fb53f09047dc0ff21a2c460ad5991b90fa1bdb7376db807dbfd@ec2-107-20-255-96.compute-1.amazonaws.com:5432/postgresql-asymmetrical-63190",
-  ssl: true,
-});
+var connectionString = "postgres://gudqbffhpdwnmx:2b1f8bb5a9e04fb53f09047dc0ff21a2c460ad5991b90fa1bdb7376db807dbfd@ec2-107-20-255-96.compute-1.amazonaws.com:5432/postgresql-asymmetrical-63190";
 
 app.get('/getPerson/:id', function(request, response) {
-    client.connect()
-    
-    client.query('SELECT * FROM messages;', (err, res) => {
+    pg.connect(connectionString, function(err, client, done){
+          client.query('SELECT * FROM messages;', (err, res) => {
         if (err) throw err;
         for (let row of res.rows) {
         console.log(JSON.stringify(row));
-        }
-        client.end();
-
-        response.render('messages', {"data": res.rows})
+      }
+    })
+       
+        response.render('pages/messages', {"data": res.rows})
 });
 });
 
