38ebfd29e8593dbf35eae26e5cdeda7f446f0a29
Hanson-Tyler <tyler.hanson123@gmail.com>
2017-11-19 00:45:46 -0800

    updating mileston

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var pg = require("pg"); // This is the postgres database connection module.
+const { Client } = require('pg');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+app.get('/getPerson/:id', function(request, response) {
+    client.connect()
+    
+    client.query('SELECT * FROM messages;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+        }
+        client.end();
+
+        response.render('messages', {"data": res.rows})
+});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
