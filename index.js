f2a464f6d2e986f2c6e780552563275ad775e2ae
Hanson-Tyler <tyler.hanson123@gmail.com>
2017-12-12 00:58:56 -0800

    project update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var bodyParser = require('body-parser');
 
 var pg = require("pg"); // This is the postgres database connection module.
 const { Client } = require('pg');
@@ -7,8 +10,9 @@ const { Client } = require('pg');
 var connectionString = process.env.DATABASE_URL;
 //var connectionString = "postgres://ta_user:ta_pass@localhost:5432/inbox";
 
-
 app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser());
 
 app.use(express.static(__dirname + '/public'));
 
@@ -51,10 +55,65 @@ app.get('/getPerson/:id', function(request, response) {
         response.render('pages/messages', {"data": res.rows})
 
         client.end();
-});   
+    });   
 });
 
+app.post('/sender', function(request, response) {
+  console.log(request.body);
+  
+    const client = new pg.Client(connectionString);
+    client.connect();
+    var sql = "SELECT * FROM messages WHERE sender = '" + request.body.sent + "';";
+    client.query(sql, (err, res) => {
+        if (err) 
+          throw err;
+        for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+        }
 
+        response.render('pages/messages', {"data": res.rows})
+
+        client.end();
+});
+});
+
+app.post('/receiver', function(request, response) {
+  console.log(request.body);
+  
+    const client = new pg.Client(connectionString);
+    client.connect();
+    var sql = "SELECT * FROM messages WHERE recipient = '" + request.body.sent + "';";
+    client.query(sql, (err, res) => {
+        if (err) 
+          throw err;
+        for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+        }
+
+        response.render('pages/messages', {"data": res.rows})
+
+        client.end();
+});
+});
+
+app.post('/all', function(request, response) {
+  console.log(request.body);
+  
+    const client = new pg.Client(connectionString);
+    client.connect();
+    var sql = "SELECT * FROM messages;";
+    client.query(sql, (err, res) => {
+        if (err) 
+          throw err;
+        for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+        }
+
+        response.render('pages/messages', {"data": res.rows})
+
+        client.end();
+});
+});
 
 
 app.listen(app.get('port'), function() {
@@ -63,3 +122,4 @@ app.listen(app.get('port'), function() {
 
 
  
+server.listen(process.env.PORT, process.env.IP);
